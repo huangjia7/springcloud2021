7a4c9f2094e2bdaafde9e24f3aeac0d3c3f8b6c3
huangjia7 <zouyi202@163.com>
2021-03-16 23:53:08 +0800

    测试

--- cloud-consumer-order80/src/main/java/com/linkus/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/linkus/springcloud/OrderMain80.java
@@ -6,11 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
+/**
+ * 预定模块
+ */
 @SpringBootApplication
 @EnableEurekaClient
 @RibbonClient(name="CLOUD-PROVIDER-SERVICE", configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
+        System.out.println("brgin--");
         SpringApplication.run(OrderMain80.class,args);
     }
 }
